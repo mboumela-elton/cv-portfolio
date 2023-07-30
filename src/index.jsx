e7158ea40473650a615bb53fb95b30710cbb417f
Mboumela Elton <mboumelae@gmail.com>
2023-07-30 07:44:21 +0100

    test

--- src/index.jsx
+++ src/index.jsx
@@ -2,21 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/';
-import ReactGA from 'react-ga';
-
-ReactGA.initialize('G-DFG06PQW1N');
-
-const logPageView = () => {
-  ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname);
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} onEnter={logPageView} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   </React.StrictMode>,
